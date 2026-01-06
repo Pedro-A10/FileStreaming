0df584e26a7228838a72a10a1a0d94e5b85d26dd
Pedro-A10 <pedroaugusto732@gmail.com>
2026-01-06 20:22:10 -0300

    Initial commit

---
+++ src/main/java/com/pedroa10/FileStreaming/FileStreamingApplication.java
@@ -0,0 +1,13 @@
+package com.pedroa10.FileStreaming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileStreamingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileStreamingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pedroa10/FileStreaming/FileStreamingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pedroa10.FileStreaming;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileStreamingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
